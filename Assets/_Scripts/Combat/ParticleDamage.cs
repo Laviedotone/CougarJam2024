0bb9eb36e2cec1571d9b9a0a95cdd34c74b46b0d
Laviedotone <laviedotone@gmail.com>
2024-10-19 21:56:41 -0500

    Update ParticleDamage.cs
    no changes made.

--- Assets/_Scripts/Combat/ParticleDamage.cs
+++ Assets/_Scripts/Combat/ParticleDamage.cs
@@ -18,7 +18,7 @@ public class ParticleDamage : MonoBehaviour
                 }else{
                     playerHealth.TakeDamage(damage);
                 }
-                
+            
             }
         }
     }
